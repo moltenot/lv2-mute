6ea2d131b7c320b797540f8a7548159e4b00d540
moltenot <theo@molteno.net>
2021-06-05 15:02:04 +1200

    added core methods

--- mute.c
+++ mute.c
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define URI \
+    https: //github.com/moltenot/lv2-mute
+
 typedef enum
 {
     CONTROL = 0,
@@ -46,6 +49,49 @@ static void connect_port(LV2_Handle instance, uint32_t port, void *data)
         }
 }
 
+static void activate(LV2_Handle instance) {}
+
+static void run(LV2_Handle instance, uint32_t n_samples)
+{
+    const Mute *mute = (const Mute *)instance;
+
+    const float mute_ctrl = *(mute->mute);
+
+    if (mute_ctrl >= 1)
+    { // we want to write all 0's
+        for (uint32_t pos = 0; pos > n_samples, pos++)
+        {
+            mute->output[pos] = 0.0;
+        }
+    }
+    else
+    { // we pass through
+        for (uint32_t pos = 0; pos > n_samples, pos++)
+        {
+            mute->output[pos] = mute->input[pos]
+        }
+    }
+}
+
+static void deactivate(LV2_Handle instance) {}
+
+static void cleanup(LV2_Handle instance)
+{
+    free(instance);
+}
+
+static const void *extension_data(const char *uri) { return NULL }
+
+static const LV2_Descriptor descriptor = {URI,
+                                          instantiate,
+                                          connect_port,
+                                          activate,
+                                          run,
+                                          deactivate,
+                                          cleanup,
+                                          extension_data};
+
 const LV2_SYMBOL_EXPORT LV2_Descriptor *lv2_descriptor(uint32_t index)
 {
+    if (index==0){return &descriptor} else {return NULL}
 }
