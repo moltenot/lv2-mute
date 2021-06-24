02fbdf69a6692db800c3394cc4cdbb14c8d1cd90
Theo Molteno <theo@molteno.net>
2021-06-24 23:10:50 +1200

    changed to toggle button

--- mute_gui.cpp
+++ mute_gui.cpp
@@ -3,15 +3,31 @@
 
 using namespace Gtk;
 
+// http://ll-plugins.nongnu.org/dox/lv2-c++-tools/1.0.5/classLV2_1_1GUI.html
 class MuteGUI : public LV2::GUI<MuteGUI>
 {
 public:
     MuteGUI(const std::string &URI)
     {
-        mute_button.add_label("mute button");
-        mute_button.set_size_request(100,100);
+        set_border_width(10);
 
-        // would regester a signal handler here
+        mute_button.add_label("mute");
+        mute_button.set_size_request(100,50);
+
+        sigc::slot<void> slot = 
+                sigc::compose(
+                    sigc::bind(
+                        mem_fun(*this, &MuteGUI::write_control), 0),                        ),
+                        mem_fun(*mute_button,)
+                    )
+                );
+
+        mute_button->on_button_press_event().connect(sigc::mem_fun(*this, &MuteGUI::write_control))
+        // toggle button reference:
+        // https://developer.gnome.org/gtkmm/stable/classGtk_1_1ToggleButton.html
+
+        // How the nongnu guy does it
+        // http://ll-plugins.nongnu.org/lv2pftci/#A_GUI
 
         mute_button.show();
         add(mute_button);
@@ -20,7 +36,7 @@ public:
     // void port_event(uint32_t port, uint32_t buffer_size,
     //                 uint32_t format, void const *buffer) {}
 protected:
-    Gtk::Button mute_button;
+    Gtk::ToggleButton mute_button;
 };
 
 static int _ = MuteGUI::register_class("https://github.com/moltenot/lv2-mute/wiki");
